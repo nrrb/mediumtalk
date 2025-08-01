4c5e45b49f8dd62d31098c9c3c2b49edac21622c
Nicholas Bennett <nbennett@cybermaxx.com>
2025-08-01 13:22:22 -0400

    fix: Correct vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: { port: 5190 },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    outDir: 'docs',
-  },
-  base: mode === 'production' ? '/mediumtalk/' : '/'
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/mediumtalk/' : '/',
+    plugins: [vue()],
+    server: { port: 5190 },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      outDir: 'docs',
+    },
+  }
 })
